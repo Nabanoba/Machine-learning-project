d6c4e37f1ddca1c54752bf39d7b1dbafd7aecdb5
nananoba yunia <nanyunjayunia@gmail.com>
2026-04-30 22:13:17 +0300

    fix: resolve duplicate SQLAlchemy instance issue

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app) 
-db = SQLAlchemy(app)
 
 with app.app_context():
     db.create_all()
